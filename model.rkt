0a6bfd994e412a88c0514146a3bdebaa5af4a693
Leyla <lyatifiva.leyla@gmail.com>
2026-05-18 09:24:11 -0500

    Some changes

--- model.rkt
+++ model.rkt
@@ -5,11 +5,10 @@
 
 (define-language FREEZE
     ; FIGURE 4
-    ; TODO: define 'reg'
-    (stmt ::= (reg = inst) (br op, label, label) (store op, op))
+    (stmt ::= (Name = inst) (br op Name Name) (store op op))
 
-    (inst ::= (binop attr op, op) (conv op)(bitcast op) (select op, op, op) (icmp cond, op, op) (phi ty, (op, label) ...) 
-    (freeze op) (getelementptr op, op ..., op) (load op) (extractelement op, constant) (insertelement op, op, constant))
+    (inst ::= (binop attr op op) (conv op)(bitcast op) (select op op op) (icmp cond op op) (phi ty (op label) ...) 
+    (freeze op) (getelementptr op op ... op) (load op) (extractelement op integer) (insertelement op op integer))
 
     (cond ::= eq ne ugt uge slt sle)
 
@@ -19,24 +18,26 @@
 
     (attr ::= nsw nuw exact)
 
-    (op ::= Name constant poison)
+    (op ::= Name integer poison)
 
     (conv ::= zext sext trunc)
     
 
 
     ; SECTION 4.2
-    ( ::= )
+    ;([isz] ::= )
 
-    ( ::= )
+    ;([ptr ty]::= )
 
-    ( ::= )
+    ;([sz x ty] ::= )  TODO
 
-    (Mem ::= )
+    ;(Mem ::= () ...)  TODO
 
-    (Name ::= ,(string-append "%" (term variable-not-otherwise-mentioned)))
+    ; TODO: side-condition for Name
+    (Name ::= string)
+    
 
-    (Reg ::= (Name, (ty, v) ...) ...)
+    (Reg ::= ((Name (ty v)) ...))
 
 
     
@@ -53,6 +54,25 @@
     ;; CHECK OF WHETHER SIZE FITS WITHIN 32/64 BITS
     [(Num size x) true
      (side-condition ,(< (term i) (expt 2 (term size))))
+     (side-condition ,(< (term size) 32))
     ]
-    [else (false)]
+    [(Num size x) (raise "Size cannot be larger than 32")
+     (side-condition ,(>= (term size) 32))
+    ]
+
+    [(Num size x) (false)]
 )
+
+(define-metafunction FREEZE
+    is_Name : string -> boolean
+
+    [(is_Name str) true
+     (side-condition ,(> (string-length str) 1))
+     (side-condition ,(string=? (substring str 0 1) "%"))
+    ]
+    [(is_Name str) false]
+)
+
+
+
+
