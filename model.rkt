8be78bfa03b35be8953472fce563f4a9cc6ea573
Leyla <lyatifiva.leyla@gmail.com>
2026-05-19 15:21:36 -0500

    .

--- model.rkt
+++ model.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
-(require redex)
+(require redex/reduction-semantics)
+(require rackunit)
 
 
 (define-language FREEZE
@@ -8,19 +9,25 @@
     (stmt ::= (Name = inst) (br op Name Name) (store op op))
 
     (inst ::= (binop attr op op) (conv op)(bitcast op) (select op op op) (icmp cond op op) (phi ty (op label) ...) 
-    (freeze op) (getelementptr op op ... op) (load op) (extractelement op integer) (insertelement op op integer))
+    (freeze op) (getelementptr op op ... op) (load op) (extractelement op const) (insertelement op op const))
 
     (cond ::= eq ne ugt uge slt sle)
 
-    (ty ::= (i natural) (ptr ty) ((i natural) ...) ((ptr ty) ...))
+    (ty ::= (i sz) (ptr ty) ((i sz) ...) ((ptr ty) ...))
+
+    ;(name nat_1 natural)
+    ;(name nat_2 natural)
 
     (binop ::= add udiv sdiv shl and or)
 
     (attr ::= nsw nuw exact)
 
-    (op ::= Name integer poison)
+    (op ::= Name const poison)
 
     (conv ::= zext sext trunc)
+
+    ; NATURALS
+    (nat sz const len ::= natural)
     
 
 
@@ -35,6 +42,10 @@
 
     ; TODO: side-condition for Name
     (Name ::= string)
+
+
+    
+    
     
 
     (Reg ::= ((Name (ty v)) ...))
@@ -49,9 +60,9 @@
 ; TODO: size <= 32
 
 (define-metafunction FREEZE
-    Num : natural natural -> boolean  
+    Num : sz const -> boolean  
     [(Num 0 x) (raise "bitwidth cannot be 0")]
-    ;; CHECK OF WHETHER SIZE FITS WITHIN 32/64 BITS
+    ;; CHECK OF WHETHER SIZE FITS WITHIN 32 BITS
     [(Num size x) (true)
      (side-condition (< (term size) 32))
      (side-condition (< (term x) (expt 2 (term size)))) ; 'x' used to be 'i' but I'm guessing that was a mistake.
@@ -64,6 +75,7 @@
     [(Num size x) (false)]
 )
 
+    ; is_Name checks whether a string refers to a variable. 
 (define-metafunction FREEZE
     is_Name : string -> boolean
 
@@ -74,6 +86,63 @@
     [(is_Name str) false]
 )
 
+
+(define-metafunction FREEZE
+    of_ty_isz : (i sz) any -> boolean
+
+    [(of_ty_isz (i sz) poison) true]
+    [(of_ty_isz (i sz) const) (true)
+     (side-condition (term (Num sz const)))
+    ]
+
+    [(of_ty_isz (i sz) x) false]
+)
+
+(define-metafunction FREEZE
+    of_ty_ptr : (ptr ty) any -> boolean
+
+
+    ; TODO check the validity of a type
+    [(of_ty_ptr (ptr ty) poison) true]
+    [(of_ty_ptr (ptr ty) const) (true)
+     (side-condition (term (Num 32 const)))
+    ]
+    [(of_ty_ptr _ _) false]
+)
+
+(define-metafunction FREEZE
+    of_ty_vec_ptr : ((ptr ty) ...) len any -> boolean
+
+    ; TODO
+    [(of_ty_vec_ptr)]
+)
+
+(define-metafunction FREEZE
+    of_ty_vec_isz : ((ptr ty) ...) any -> boolean
+
+    ; TODO
+)
+(define-metafunction FREEZE
+    of_ty_trans : ty -> (ty -> bool)
+
+    ;; TODO CLEANS UP THE MESS, 
+)
+
+(define-metafunction FREEZE
+    of_ty : ty any -> boolean
+    [(of_ty (i sz) x) (of_ty_isz x)
+     ; (side-condition ,(< (term sz) 32))
+    ]
+
+    [(of_ty (ptr ty_1) x) (of_ty_ptr x)
+     ; (side-condition ,(< (term sz) 32))  !!! ty_1 must actually exist
+    ]
+
+    [(of_ty ((i sz) ... ) x) (of_ty_vec_isz x)]
+
+    [(of_ty ((ptr ty) ... ) x) (of_ty_vec_ptr x)]
+
+)
 
 
 
